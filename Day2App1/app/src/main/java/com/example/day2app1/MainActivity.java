711c0b72bf7840b280eef3bd372cff1af36954ad
Mariam <mariam.hossam.keshk@gmail.com>
2025-04-10 17:41:04 +0200

    Day2App1: Intents, Android Views, Supporting Localization, Supporting Different Layouts

---
+++ Day2App1/app/src/main/java/com/example/day2app1/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.day2app1;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private Button btnNext;
+    private Button btnExit;
+    private EditText editMsg;
+    private EditText editPhone;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnNext = findViewById(R.id.btnNext); // references of obj created by setContentView with id
+        btnExit = findViewById(R.id.btnExit);
+        editMsg = findViewById(R.id.editMsg);
+        editPhone = findViewById(R.id.editPhone);
+        Log.i(TAG, "onCreate: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+
+    public void next(View view) {
+    }
+
+    public void exit(View view) {
+        finish();
+    }
+}
